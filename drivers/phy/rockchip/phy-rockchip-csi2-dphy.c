ef4ba44d9e87165e241b10ccda766010834eb32e
Alexander Shiyan <eagle.alexander923@gmail.com>
2026-05-29 12:42:57 +0300

    rockchip: csi2-isp: Simplify module loading

--- drivers/phy/rockchip/phy-rockchip-csi2-dphy.c
+++ drivers/phy/rockchip/phy-rockchip-csi2-dphy.c
@@ -824,19 +824,7 @@ struct platform_driver rockchip_csi2_dphy_driver = {
 		.of_match_table = rockchip_csi2_dphy_match_id,
 	},
 };
-
-int rockchip_csi2_dphy_init(void)
-{
-	return platform_driver_register(&rockchip_csi2_dphy_driver);
-}
-
-#if defined(CONFIG_VIDEO_ROCKCHIP_THUNDER_BOOT_ISP) && !defined(CONFIG_INITCALL_ASYNC)
-subsys_initcall(rockchip_csi2_dphy_init);
-#else
-#if !defined(CONFIG_VIDEO_REVERSE_IMAGE)
 module_platform_driver(rockchip_csi2_dphy_driver);
-#endif
-#endif
 
 MODULE_AUTHOR("Rockchip Camera/ISP team");
 MODULE_DESCRIPTION("Rockchip MIPI CSI2 DPHY driver");
