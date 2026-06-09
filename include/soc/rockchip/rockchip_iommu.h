27bdffabf5fe99011c3b8e239142061becb7e239
Alex <skupov93@gmail.com>
2026-06-09 16:25:36 +0500

    Added driver of RKNPU

--- include/soc/rockchip/rockchip_iommu.h
+++ include/soc/rockchip/rockchip_iommu.h
@@ -5,11 +5,26 @@
 #ifndef __SOC_ROCKCHIP_IOMMU_H
 #define __SOC_ROCKCHIP_IOMMU_H
 
+#include <linux/device.h>
+#include <linux/iommu.h>
+
 struct device;
 
 #if IS_REACHABLE(CONFIG_ROCKCHIP_IOMMU)
 int rockchip_iommu_enable(struct device *dev);
 int rockchip_iommu_disable(struct device *dev);
+
+static inline bool rockchip_iommu_is_enabled(struct device *dev)
+{
+	/* If the device has an IOMMU group, IOMMU is enabled */
+	struct iommu_group *group = iommu_group_get(dev);
+	if (group) {
+		iommu_group_put(group);
+		return true;
+	}
+	return false;
+}
+
 #else
 static inline int rockchip_iommu_enable(struct device *dev)
 {
